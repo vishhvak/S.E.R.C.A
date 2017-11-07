25647a160498347ec7ff426cc930105d8b92eb93
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-11-07 21:36:21 +0530

    Hello there

--- home/views.py
+++ home/views.py
@@ -7,4 +7,11 @@ def form(request):
 	return render(request, 'form.html')
 
 def graph(request):
-	return render(request, 'graph.html')
+	if request.method == 'POST':
+		lights = request.POST['lights']
+		if lights:
+			print(lights)
+		return render(request, 'graph.html', 
+			{'lights': lights} )
+	else:
+		return render(request, 'graph.html')
