8328e2d6f21025661f4d7b725b2827e327f6bff0
Vishhvak <cswithvish@gmail.com>
2017-11-09 00:33:28 +0530

    Defined Names and vars for form inputs

--- home/views.py
+++ home/views.py
@@ -9,6 +9,21 @@ def form(request):
 def graph(request):
 	if request.method == 'POST':
 		lights = request.POST['lights']
+		fans = request.POST['fans']
+		TV = request.POST['TV']
+		fridge = request.POST['fridge']
+		wash = request.POST['wash']
+		AC = request.POST['AC']
+		shower = request.POST['shower']
+		rest = request.POST['rest']
+		fresh = request.POST['fresh']
+		washdish = request.POST['washdish']
+		drink = request.POST['drink']
+		lightstime = request.POST['lightstime']
+		fantime = request.POST['fantime']
+		tvtime = request.POST['tvtime']
+		ACtime = request.POST['ACtime']
+		WMtime = request.POST['WMtime']
 		if lights:
 			print(lights)
 		return render(request, 'graph.html', 
