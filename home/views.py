30ac8a8888589497f5bfeacf908f4af47a2daced
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-11-13 09:14:07 +0530

    Yolo

--- home/views.py
+++ home/views.py
@@ -14,20 +14,23 @@ def graph(request):
 		fridge = request.POST.get('fridge', False);
 		wash = request.POST.get('wash', False);
 		AC = request.POST.get('AC', False);
-		shower = request.POST.get('shower', False);
-		rest = request.POST.get('rest', False);
-		fresh = request.POST.get('fresh', False);
-		washdish = request.POST.get('washdish', False);
-		drink = request.POST.get('drink', False);
+		shower = round(int(request.POST.get('shower', False))*0.024*365);
+		rest = round(int(request.POST.get('rest', False))*0.024*365);
+		fresh = round(int(request.POST.get('fresh', False))*0.024*365);
+		washdish = round(int(request.POST.get('washdish', False))*0.024*365);
+		drink = round(int(request.POST.get('drink', False))*0.024*365);
 		lightstime = request.POST.get('lightstime', False);
 		fantime = request.POST.get('fantime', False);
 		tvtime = request.POST.get('tvtime', False);
-		ACtime = request.POST.get('ACTime', False);
+		ACtime = request.POST.get('ACtime', False);
+		Rtime = request.POST.get('Rtime', False);
 		WMtime = request.POST.get('WMTime', False);
 
-		le = int(lights)*int(lightstime)
-		fe = int(fans)*int(fantime)
-		ace = int(AC)*int(ACtime)
+		le = round(int(lights)*int(lightstime)*0.024*365)
+		fe = round(int(fans)*int(fantime)*0.035*365)
+		ace = round(int(AC)*int(ACtime)*0.542*365)
+		tve = round(int(TV)*int(tvtime)*0.234*365)
+		re = round(int(fridge)*int(Rtime)*0.234*365)
 		wme = int(wash)*int(WMtime)
 		return render(request, 'graph.html', 
 			{'lights': lights,
@@ -49,6 +52,8 @@ def graph(request):
 			 'le': le,
 			 'fe': fe,
 			 'ace': ace,
+			 'tve': tve,
+			 're': re,
 			 'wme': wme,
 			 } )
 	else:
